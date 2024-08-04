1cb591aa150dd41f53ad5840a80c87dc7f2e063b
justin-tanvt <justintan28@gmail.com>
2024-08-04 14:42:26 +0800

    Implement path compression for quick-union and weighted-quick-union

---
+++ wk01/union-find/src/main/java/WQUPCUF.java
@@ -0,0 +1,62 @@
+import edu.princeton.cs.algs4.StdIn;
+
+public class WQUUF {
+
+    private final int[] parent;
+    private final int[] size;
+
+    public WQUUF(int N) {
+        this.parent = new int[N];
+        this.size = new int[N];
+        for (int i = 0; i < N; i++) {
+            this.parent[i] = i;
+            this.size[i] = 1;
+        }
+    }
+
+    public void union(int p, int q) {
+        int rootP = root(p);
+        int rootQ = root(q);
+        if (rootP == rootQ) return;
+
+        if (size[rootP] < size[rootQ]) {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        } else {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        }
+    }
+
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    private int root(int i) {
+        while (parent[i] != i) {
+            i = parent[i];
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
+        int N = StdIn.readInt();
+        WQUUF uf = new WQUUF(N);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q)) continue;
+            uf.union(p, q);
+        }
+
+        long endTime = System.nanoTime();
+        long elapsedMilli = (endTime - startTime) / 1000000;
+        System.out.println("Weighted Quick Union-Find computation took " + elapsedMilli + "ms");
+
+//        System.out.println("parent	--> " + Arrays.toString(uf.parent));
+//        System.out.println("depth  --> " + Arrays.toString(uf.size));
+    }
+
+}
